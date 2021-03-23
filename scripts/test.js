88bf6f76423ca5df8a840407a6f02759f7e11a74
TOMMY(a.k.a JPS.) <tommy999_jps@yahoo.co.jp>
2021-03-24 01:27:03 +0900

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,10 @@
 // 外から渡す
 const fuga = process.env.hoge;
 console.log(fuga);
+
+const splited = process.env.GITHUB_REPOSITORY.split("/");
+const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split("/");
+
+console.log(splited);
+console.log(owner);
+console.log(repo);
